78d3067237374c7cf99fb0025bad18202a4208a4
Bagus Isrovi <muhammad.bagus@meteor.id>
2023-05-11 16:21:19 +0700

    change port

--- app.js
+++ app.js
@@ -10,5 +10,5 @@ app.use(express.urlencoded({ extended: true }));
 app.use('/', routes);
 
 app.listen(process.env.PORT || 3000, () => {
-  console.log(`Server is running on port ${PORT}`);
+  console.log(`Server is running on port ${process.env.PORT}`);
 });
